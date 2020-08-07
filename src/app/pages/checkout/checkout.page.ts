94e800aa22a4cd6f25c1d713e0e65901cd6ed543
Anderson Laverde Gracia <ander.laverde.dev@gmail.com>
2020-08-07 12:44:16 -0500

    feat(Checkout): dismiss modal in phones

--- src/app/pages/checkout/checkout.page.ts
+++ src/app/pages/checkout/checkout.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { IRoom, IAditional } from 'src/app/tab1/domain/responses.interface';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-checkout',
@@ -11,10 +12,20 @@ export class CheckoutPage implements OnInit {
   @Input() room: IRoom;
   @Input() aditionals: IAditional;
   
-  constructor() { }
+  constructor(
+    public modalController: ModalController
+  ) { }
 
   ngOnInit() {
     console.log(this.room, this.aditionals);
+  }
+
+  dismiss() {
+    // using the injected ModalController this page
+    // can "dismiss" itself and optionally pass back data
+    this.modalController.dismiss({
+      'dismissed': true
+    });
   }
 
 }
