c6428e7c8f28aa1529a4515380ccb5fee7e79bab
Anderson Laverde Gracia <ander.laverde.dev@gmail.com>
2020-08-07 11:20:07 -0500

    feat(Rooms) : new room detail page

---
+++ src/app/pages/room-detail/room-detail.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-room-detail',
+  templateUrl: './room-detail.page.html',
+  styleUrls: ['./room-detail.page.scss'],
+})
+export class RoomDetailPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
